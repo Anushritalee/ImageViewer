632c296ff0be405c1341242ea43971ff23705cf8
Anushrita Anshul Lee <66158280+Anushritalee@users.noreply.github.com>
2021-04-26 23:59:17 +0530

    profile picture has been added

--- src/common/searchBox/SearchBox.js
+++ src/common/searchBox/SearchBox.js
@@ -4,21 +4,52 @@ import { Input, InputAdornment } from "@material-ui/core";
 import "./SearchBox.css";
 
 export default class SearchBox extends Component {
-  render() {
-    return (
-      <div className="header-right-flex-container">
-        <Input
-          className="search-box"
-          type="search"
-          placeholder="Search..."
-          disableUnderline
-          startAdornment={
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          }
-        />
-      </div>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+          filtered_post: null,
+          searchText: "",
+        };
+      }
+    
+      onSearch = (e) => {
+        console.log("started search");
+        this.setState({
+          searchText: e.target.value,
+        });
+        if (this.state.searchText == null || this.state.searchText.trim() === "") {
+          this.setState({ filtered_post: this.props.allPosts });
+        } else {
+          let filteredRecentPosts = this.props.allPosts.filter((ele) => {
+            return ele.caption.includes(this.state.searchText);
+          });
+          console.log(filteredRecentPosts);
+        }
+        console.log("completed search");
+      };
+    
+      componentDidCatch() {
+        this.setState({ filtered_post: this.props.allPosts });
+        console.log("From mount\n\n", this.state.filtered_post);
+      }
+    
+      render() {
+        console.log("Render");
+        return (
+          <div className="header-right-flex-container">
+            <Input
+              className="search-box"
+              type="search"
+              placeholder="Search..."
+              disableUnderline
+              onChange={this.onSearch}
+              startAdornment={
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              }
+            />
+          </div>
+        );
+      }
+    }
