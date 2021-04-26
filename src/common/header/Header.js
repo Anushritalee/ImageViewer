4839bf25a14a2242d0dc6c7db62737dc9624f4aa
Anushrita Anshul Lee <66158280+Anushritalee@users.noreply.github.com>
2021-04-26 22:30:26 +0530

    Added profile icon and logic to retrieve the pic

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,11 +1,46 @@
-import React, { Component } from "react";
+
+import React, { Component, Fragment } from "react";
+import SearchIcon from "@material-ui/icons/Search";
 import "./Header.css";
+import { Avatar, IconButton, Input, InputAdornment } from "@material-ui/core";
 
 class Header extends Component {
   render() {
     return (
       <header className="app-header">
-        <span className="header-logo-text">Image Viewer</span>
+        {this.props.isLoggedIn !== true ? (
+          <div>
+            <span className="header-logo-text">Image Viewer</span>
+          </div>
+        ) : (
+          <div>
+            <span className="header-logo-text">Image Viewer</span>
+            <Fragment>
+              <div>
+                <header className="logo">Image Viewer</header>
+              </div>
+              <div className="header-right-flex-container">
+                <Input
+                  className="search-box"
+                  type="search"
+                  placeholder="Search..."
+                  disableUnderline
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  }
+                />
+                 <IconButton>
+                  <Avatar
+                    alt="profile_picture"
+                    src={this.props.profilePictureUrl}
+                  />
+                </IconButton>
+              </div>
+            </Fragment>
+          </div>
+        )}
       </header>
     );
   }
